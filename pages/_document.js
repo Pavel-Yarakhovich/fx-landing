d0fd13d8c65fcaf3071885a4aebebc5117011d63
Pavel <pavel.yarri@gmail.com>
2021-08-29 14:45:04 +0300

    fb verification tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class WebDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-            <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K7TH73T');`}}></script>
+            })(window,document,'script','dataLayer','GTM-K7TH73T');`,
+            }}
+          ></script>
+          <meta
+            name="facebook-domain-verification"
+            content="u4x39zmqdoskbo22up3lt40lvl11h9"
+          />
         </Head>
         <body>
-            <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7TH73T"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
-          <Main/>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7TH73T"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
+          <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
-export default WebDocument
+export default WebDocument;
